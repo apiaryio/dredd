14ad009bb5bc2bdd454777c751ce9c76be21ef4d
Honza Javorek <mail@honzajavorek.cz>
2017-11-06 16:36:14 +0100

    chore: Use random git magic to find relevant commit range

--- scripts/commitlint.sh
+++ scripts/commitlint.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 # Validates format of the commit messages on Travis CI
 
-# Copyright (c) 2016 Mario Nebl
-# License: MIT (https://github.com/marionebl/commitlint/blob/master/license.md)
-# Source: https://marionebl.github.io/commitlint/#/guides-ci-setup
 
-set -e
-set -u
+set -e # aborts as soon as anything returns non-zero exit status
 
 
-if [[ $TRAVIS_PULL_REQUEST_SLUG != "" && $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG ]]; then
-    # This is a Pull Request from a different slug, hence a forked repository
-    git remote add "$TRAVIS_PULL_REQUEST_SLUG" "https://github.com/$TRAVIS_PULL_REQUEST_SLUG.git"
-    git fetch "$TRAVIS_PULL_REQUEST_SLUG"
+git remote set-branches origin master
+git fetch --unshallow --quiet
+git checkout master --quiet
+git checkout - --quiet
 
-    # Use the fetched remote pointing to the source clone for comparison
-    TO="$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH"
-else
-    # This is a Pull Request from the same remote, no clone repository
-    TO=$TRAVIS_COMMIT
-fi
-
-# Lint all commits in the PR
-# - Covers fork pull requests (when TO=slug/branch)
-# - Covers branch pull requests (when TO=branch)
-./node_modules/.bin/commitlint --from="$TRAVIS_BRANCH" --to="$TO"
-
-# Always lint the triggerig commit
-# - Covers direct commits
-./node_modules/.bin/commitlint --from="$TRAVIS_COMMIT"
+./node_modules/.bin/commitlint --from=master --to="$TRAVIS_COMMIT"
