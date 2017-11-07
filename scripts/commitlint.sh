5f52738ba61f7f3354c70107e2d37d11c9956641
Honza Javorek <mail@honzajavorek.cz>
2017-11-07 15:21:21 +0100

    chore: Make npm run lint worky locally

--- scripts/commitlint.sh
+++ scripts/commitlint.sh
@@ -5,9 +5,12 @@
 set -e # aborts as soon as anything returns non-zero exit status
 
 
-git remote set-branches origin master
-git fetch --unshallow --quiet
-git checkout master --quiet
-git checkout - --quiet
-
-./node_modules/.bin/commitlint --from=master --to="$TRAVIS_COMMIT"
+if [ ! -z "$TRAVIS_COMMIT" ]; then
+    git remote set-branches origin master
+    git fetch --unshallow --quiet
+    git checkout master --quiet
+    git checkout - --quiet
+    ./node_modules/.bin/commitlint --from=master --to="$TRAVIS_COMMIT"
+else
+    ./node_modules/.bin/commitlint --from=master
+fi
