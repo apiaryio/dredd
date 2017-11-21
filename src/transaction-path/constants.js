150d0cb002cc3ee2cbfe0dd54050f4d43453391d
michalholasek <michal.holasek@apiary.io>
2017-11-21 13:56:21 +0100

    chore: Remove unnecessary comment from constants.js

--- src/transaction-path/constants.js
+++ src/transaction-path/constants.js
@@ -1,8 +1,5 @@
 module.exports = {
   DELIMITER: ':',
-
-  // This is in fact a single backslash character, but in stupid JS it's two
-  // because you're escaping a JS escape character \.
   ESCAPE_CHAR: '\\',
   MAX_PARTS: 5
 };
