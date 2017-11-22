88b8b3f3ac3927c8b0b0a5ee9febf39579d17a0b
michalholasek <michal.holasek@apiary.io>
2017-11-22 12:02:28 +0100

    chore: Make decaffed tests and coverage work

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -9,21 +9,17 @@ PROJECT_DIR=$(pwd -P)
 COVERRAGE_DIR="$PROJECT_DIR"/coverage
 COVERAGE_FILE="$PROJECT_DIR"/coverage/lcov.info
 BIN_DIR="$PROJECT_DIR"/node_modules/.bin
-TEST_GLOB=dist/test/**/*-test.coffee
+TEST_GLOB="$PROJECT_DIR"/dist/test/**/*-test.js
 
 
 # Cleanup & preparation
 rm -rf "$COVERRAGE_DIR"
 rm -rf dist
-npm run copy:test
-npm run build
+npm run make
 
 
 # Testing
 export COVERAGE_FILE
-istanbul cover ./node_modules/.bin/mocha -- \
-    "$TEST_GLOB" \
-    --compilers=coffee:coffee-script/register \
-    --recursive
+istanbul cover ./node_modules/.bin/_mocha -- "$TEST_GLOB" --opts=./mocha.opts
 cd ..
 echo "Coverage saved as '$COVERAGE_FILE'"
