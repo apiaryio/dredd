bd1c4d4dd070ec195cf390cf370072320a845309
michalholasek <michal.holasek@apiary.io>
2017-12-18 15:09:35 +0100

    chore: Add eslint and Airbnb styleguide

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb/base',
+  env: {
+    'browser': true,
+    'mocha': true,
+    'node': true
+  },
+  rules: {
+    'comma-dangle': ['error', 'never']
+  }
+};
