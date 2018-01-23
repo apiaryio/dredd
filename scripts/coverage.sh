24725e054e1a6b3873b8389d667a801635c6f88b
michalholasek <michal.holasek@apiary.io>
2018-01-23 14:37:44 +0100

    chore: Modify coverage scripts to reflect decaff changes

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,44 +1,28 @@
 #!/bin/sh
-# Runs tests and collects code coverage to ./cov.info file
+# Runs tests and collects code coverage to ./coverage/lcov.info file
 
-set -e # aborts as soon as anything returns non-zero exit status
+set -e # Aborts as soon as anything returns non-zero exit status
 
 
 PROJECT_DIR=$(pwd -P)
 
-COVERAGE_FILE="$PROJECT_DIR"/cov.info
-COVERAGE_DIR="$PROJECT_DIR"/lcov
-INSTRUMENTED_CODE_DIR="$PROJECT_DIR"/src-cov
+COVERAGE_FILE="$PROJECT_DIR"/coverage/lcov.info
+COVERAGE_DIR="$PROJECT_DIR"/coverage
 BIN_DIR="$PROJECT_DIR"/node_modules/.bin
 
 
 # Cleanup & preparation
-rm -rf "$INSTRUMENTED_CODE_DIR" "$COVERAGE_DIR" "$COVERAGE_FILE"
-mkdir "$INSTRUMENTED_CODE_DIR" "$COVERAGE_DIR"
+rm -rf "$COVERAGE_DIR" "$COVERAGE_FILE"
+mkdir "$COVERAGE_DIR"
+chmod +x "$PROJECT_DIR"/bin/dredd
 
+# Testing & coverage
+istanbul cover ./node_modules/.bin/_mocha -- \
+    "test/**/*-test.coffee" \
+    --compilers=coffee:coffee-script/register \
+    --recursive
 
-# Creating directory with instrumented JS code
-"$BIN_DIR"/coffeeCoverage \
-    --exclude=node_modules,.git,test,scripts \
-    --path=relative \
-    "$PROJECT_DIR" "$INSTRUMENTED_CODE_DIR" 1>&2
-
-cp "$PROJECT_DIR"/package.json "$INSTRUMENTED_CODE_DIR"
-
-cp -r "$PROJECT_DIR"/bin "$INSTRUMENTED_CODE_DIR"/bin
-chmod +x "$INSTRUMENTED_CODE_DIR"/bin/*
-
-cp -r "$PROJECT_DIR"/scripts "$INSTRUMENTED_CODE_DIR"/scripts
-cp -r "$PROJECT_DIR"/docs "$INSTRUMENTED_CODE_DIR"/docs
-cp -r "$PROJECT_DIR"/test "$INSTRUMENTED_CODE_DIR"/test
-
-mkdir -p "$INSTRUMENTED_CODE_DIR"/node_modules/.bin
-ln -s "$BIN_DIR"/* "$INSTRUMENTED_CODE_DIR"/node_modules/.bin
-
-
-# Testing
 export COVERAGE_DIR
-cd "$INSTRUMENTED_CODE_DIR" && npm test
 cd ..
 
 
@@ -48,4 +32,4 @@ echo "Coverage saved as '$COVERAGE_FILE'"
 
 
 # Output & cleanup
-rm -rf "$INSTRUMENTED_CODE_DIR" "$COVERAGE_DIR"
+rm -rf "$COVERAGE_DIR"
