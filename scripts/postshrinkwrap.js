80accd1507df2ec0d0d7cf50f7c79633e9c32889
Honza Javorek <mail@honzajavorek.cz>
2019-01-07 17:23:29 +0100

    chore: remove shrinkwrap altering except of https enforcement

--- scripts/postshrinkwrap.js
+++ scripts/postshrinkwrap.js
@@ -1,21 +1,9 @@
 #!/usr/bin/env node
-// Postprocesses the lockfile to remove dev dependencies and one C++
-// dependency, and to enforce https://
+// Postprocesses the lockfile to enforce https://
 
 const fs = require('fs');
 const path = require('path');
 const lockfile = require('../npm-shrinkwrap');
-
-// Remove dev dependencies so they NEVER get to the final distribution of Dredd
-Object.keys(lockfile.dependencies)
-  .filter(name => lockfile.dependencies[name].dev)
-  .forEach(name => delete lockfile.dependencies[name]);
-
-// Force all installations of Dredd to use only the pure JavaScript version
-// of the API Blueprint parser. It has slower performance, but it solves
-// quite a few installation & distribution problems.
-delete lockfile.dependencies.drafter.requires.protagonist;
-delete lockfile.dependencies.protagonist;
 
 const json = JSON.stringify(lockfile, null, 2)
   // Sometimes the npm registry resolves to http:// URLs for no obvious reason.
