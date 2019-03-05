0fd09377220bdfd0d61c5ebef9ee4629e62a4841
Honza Javorek <mail@honzajavorek.cz>
2019-03-05 16:44:52 +0100

    refactor: use 'resolveHookfiles' for API description paths

--- test/unit/Dredd-test.js
+++ test/unit/Dredd-test.js
@@ -22,6 +22,9 @@ const Dredd = proxyquire('../../lib/Dredd', {
 });
 
 
+const CWD = process.cwd();
+
+
 function compareLocation(ad1, ad2) {
   if (ad1.location < ad2.location) { return -1; }
   if (ad1.location > ad2.location) { return 1; }
@@ -61,7 +64,7 @@ describe('Dredd class', () => {
       dredd = new Dredd(configuration);
       sinon.stub(dredd.runner, 'executeTransaction').callsFake((transaction, hooks, callback) => callback());
       dredd.run(() => {
-        assert.isOk(fsStub.readFile.calledWith(configuration.options.path[0]));
+        assert.isOk(fsStub.readFile.calledWith(`${CWD}/test/fixtures/apiary.apib`));
         dredd.runner.executeTransaction.restore();
         done();
       });
@@ -117,16 +120,16 @@ describe('Dredd class', () => {
         if (error) { return done(error); }
         assert.lengthOf(dredd.files, 3);
         assert.deepEqual(dredd.files, [
-          './test/fixtures/multifile/greeting.apib',
-          './test/fixtures/multifile/message.apib',
-          './test/fixtures/multifile/name.apib',
+          `${CWD}/test/fixtures/multifile/greeting.apib`,
+          `${CWD}/test/fixtures/multifile/message.apib`,
+          `${CWD}/test/fixtures/multifile/name.apib`,
         ]);
         done();
       }));
 
       it('should remove globs from config', done => dredd.run((error) => {
         if (error) { return done(error); }
-        assert.notInclude(dredd.files, './test/fixtures/multifile/*.apib');
+        assert.notInclude(dredd.files, 'test/fixtures/multifile/*.apib');
         done();
       }));
 
@@ -136,15 +139,15 @@ describe('Dredd class', () => {
         dredd.configuration.apiDescriptions.sort(compareLocation);
 
         assert.isObject(dredd.configuration.apiDescriptions[0]);
-        assert.propertyVal(dredd.configuration.apiDescriptions[0], 'location', './test/fixtures/multifile/greeting.apib');
+        assert.propertyVal(dredd.configuration.apiDescriptions[0], 'location', `${CWD}/test/fixtures/multifile/greeting.apib`);
         assert.property(dredd.configuration.apiDescriptions[0], 'content');
 
         assert.isObject(dredd.configuration.apiDescriptions[1]);
-        assert.propertyVal(dredd.configuration.apiDescriptions[1], 'location', './test/fixtures/multifile/message.apib');
+        assert.propertyVal(dredd.configuration.apiDescriptions[1], 'location', `${CWD}/test/fixtures/multifile/message.apib`);
         assert.property(dredd.configuration.apiDescriptions[1], 'content');
 
         assert.isObject(dredd.configuration.apiDescriptions[2]);
-        assert.propertyVal(dredd.configuration.apiDescriptions[2], 'location', './test/fixtures/multifile/name.apib');
+        assert.propertyVal(dredd.configuration.apiDescriptions[2], 'location', `${CWD}/test/fixtures/multifile/name.apib`);
         assert.property(dredd.configuration.apiDescriptions[2], 'content');
         done();
       }));
@@ -293,7 +296,7 @@ GET /url
           assert.property(localdredd.configuration.apiDescriptions[1], 'annotations');
 
           assert.isObject(localdredd.configuration.apiDescriptions[2]);
-          assert.propertyVal(localdredd.configuration.apiDescriptions[2], 'location', './test/fixtures/apiary.apib');
+          assert.propertyVal(localdredd.configuration.apiDescriptions[2], 'location', `${CWD}/test/fixtures/apiary.apib`);
           assert.property(localdredd.configuration.apiDescriptions[2], 'content');
           assert.property(localdredd.configuration.apiDescriptions[2], 'annotations');
           done();
@@ -336,16 +339,16 @@ GET /url
           assert.deepEqual(dredd.files, [
             'http://some.path.to/file.apib',
             'https://another.path.to/apiary.apib',
-            './test/fixtures/multifile/greeting.apib',
-            './test/fixtures/multifile/message.apib',
-            './test/fixtures/multifile/name.apib',
+            `${CWD}/test/fixtures/multifile/greeting.apib`,
+            `${CWD}/test/fixtures/multifile/message.apib`,
+            `${CWD}/test/fixtures/multifile/name.apib`,
           ]);
           done();
         }));
 
         it('should remove globs from config', done => dredd.run((error) => {
           if (error) { return done(error); }
-          assert.notInclude(dredd.files, './test/fixtures/multifile/*.apib');
+          assert.notInclude(dredd.files, 'test/fixtures/multifile/*.apib');
           done();
         }));
 
@@ -356,17 +359,17 @@ GET /url
           dredd.configuration.apiDescriptions.sort(compareLocation);
 
           assert.isObject(dredd.configuration.apiDescriptions[0]);
-          assert.propertyVal(dredd.configuration.apiDescriptions[0], 'location', './test/fixtures/multifile/greeting.apib');
+          assert.propertyVal(dredd.configuration.apiDescriptions[0], 'location', `${CWD}/test/fixtures/multifile/greeting.apib`);
           assert.property(dredd.configuration.apiDescriptions[0], 'content');
           assert.property(dredd.configuration.apiDescriptions[0], 'annotations');
 
           assert.isObject(dredd.configuration.apiDescriptions[1]);
-          assert.propertyVal(dredd.configuration.apiDescriptions[1], 'location', './test/fixtures/multifile/message.apib');
+          assert.propertyVal(dredd.configuration.apiDescriptions[1], 'location', `${CWD}/test/fixtures/multifile/message.apib`);
           assert.property(dredd.configuration.apiDescriptions[1], 'content');
           assert.property(dredd.configuration.apiDescriptions[1], 'annotations');
 
           assert.isObject(dredd.configuration.apiDescriptions[2]);
-          assert.propertyVal(dredd.configuration.apiDescriptions[2], 'location', './test/fixtures/multifile/name.apib');
+          assert.propertyVal(dredd.configuration.apiDescriptions[2], 'location', `${CWD}/test/fixtures/multifile/name.apib`);
           assert.property(dredd.configuration.apiDescriptions[2], 'content');
           assert.property(dredd.configuration.apiDescriptions[2], 'annotations');
 
