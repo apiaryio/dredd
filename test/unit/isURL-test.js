67a8109ed972235fbec82939a0dc228bb4b1d4a5
Honza Javorek <mail@honzajavorek.cz>
2019-03-22 19:19:56 +0100

    test: unit test isURL()

---
+++ test/unit/isURL-test.js
@@ -0,0 +1,26 @@
+const { assert } = require('chai');
+
+const isURL = require('../../lib/isURL');
+
+
+describe('isURL()', () => {
+  it('recognizes HTTP URL', () => {
+    assert.isTrue(isURL('http://example.com'));
+  });
+
+  it('recognizes HTTPS URL', () => {
+    assert.isTrue(isURL('https://example.com'));
+  });
+
+  it('returns false for UNIX paths', () => {
+    assert.isFalse(isURL('/home/honza'));
+  });
+
+  it('returns false for Windows paths', () => {
+    assert.isFalse(isURL('C:\\Users\\Honza'));
+  });
+
+  it('returns false for file://', () => {
+    assert.isFalse(isURL('file:///home/honza'));
+  });
+});
