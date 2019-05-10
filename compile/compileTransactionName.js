7d3b698ed179fbceff8b9f874389fd58fb0f06d8
Honza Javorek <mail@honzajavorek.cz>
2019-05-10 13:59:48 +0200

    refactor: use simpler code to construct the transaction name
    Close https://github.com/apiaryio/dredd-transactions/issues/283

--- compile/compileTransactionName.js
+++ compile/compileTransactionName.js
@@ -1,9 +1,11 @@
 module.exports = function compileTransactionName(origin) {
-  const segments = [];
-  if (origin.apiName) { segments.push(origin.apiName); }
-  if (origin.resourceGroupName) { segments.push(origin.resourceGroupName); }
-  if (origin.resourceName) { segments.push(origin.resourceName); }
-  if (origin.actionName) { segments.push(origin.actionName); }
-  if (origin.exampleName) { segments.push(origin.exampleName); }
-  return segments.join(' > ');
+  return [
+    origin.apiName,
+    origin.resourceGroupName,
+    origin.resourceName,
+    origin.actionName,
+    origin.exampleName,
+  ]
+    .filter(Boolean)
+    .join(' > ');
 };
