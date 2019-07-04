f3b64fe23eb715386a254259e0f1307ceacbf85a
Honza Javorek <mail@honzajavorek.cz>
2019-07-04 18:24:42 +0200

    === wip ===

--- features/step_definitions/hooks-js.js
+++ features/step_definitions/hooks-js.js
@@ -1,42 +1,43 @@
-const { spawn } = require('child_process');
-const { expect } = require('chai');
 const { Given, When, Then } = require('cucumber');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const Dredd = require('../../lib/Dredd');
 
 
-Given('an API description with transactions "GET /articles" and "POST /articles"', function () {
-  this.dredd.apiDescription = `test/fixtures/blog/apidesc${this.apiDescriptionExt}`;
+Given('I have an API description with transactions "GET /articles" and "POST /articles"', function () {
+  this.config.path = `test/fixtures/blog/apidesc${this.apiDescriptionExt}`;
 });
 
-Given('an implementation, which requires authorization on "POST /articles"', function () {
-  this.dredd.args.push('--server=node test/fixtures/blog/app.js');
+Given('I have an implementation, which requires auth on "POST /articles"', function () {
+  this.config.options.server = 'node test/fixtures/blog/app.js';
 });
 
-Given('I have hooks adding authorization to "POST /articles"', function () {
-  this.dredd.args.push(`--hookfiles=test/fixtures/blog/hooks${this.hooksExt}`);
+Given('I have hooks adding auth to "POST /articles"', function () {
+  this.config.hookfiles = `test/fixtures/blog/hooks${this.hooksExt}`;
 });
 
 When('I run Dredd', { timeout: 10 * 1000 }, function (callback) {
-  const args = [
-    'bin/dredd',
-    this.dredd.apiDescription,
-    this.dredd.apiLocation,
-    '--color=false',
-  ].concat(this.dredd.args);
-
-  const cli = spawn('node', args);
-  cli.stdout.on('data', (data) => { this.dredd.output += data; });
-  cli.stderr.on('data', (data) => { this.dredd.output += data; });
-  cli.on('exit', (exitStatus) => { this.dredd.exitStatus = exitStatus; callback(); });
+  this.dredd = new Dredd(this.config);
+  this.dredd.run((error, stats) => {
+    this.error = error;
+    this.stats = stats;
+    callback();
+  });
 });
 
 Then('the "GET /articles" test passes', function () {
-  expect(this.dredd.output).to.contain('pass: GET (200) /articles');
+  // expect(this.config.emitter).to.contain('pass: GET (200) /articles');
+  console.log(this.config.emitter.emit.getCalls());
+  expect(this.config.emitter.emit.calledWithExactly('test pass', sinon.match())).to.be.true;
 });
 
 Then('the "POST /articles" test passes', function () {
-  expect(this.dredd.output).to.contain('pass: POST (201) /articles');
+  // expect(this.config.emitter).to.contain('pass: POST (201) /articles');
+  expect(this.config.emitter.emit.calledWithExactly('test pass', 'foo')).to.be.true;
 });
 
 Then('the "POST /articles" test fails', function () {
-  expect(this.dredd.output).to.contain('fail: POST (201) /articles');
+  // expect(this.config.emitter).to.contain('fail: POST (201) /articles');
+  expect(this.config.emitter.emit.calledWithExactly('test pass', 'foo')).to.be.true;
 });
--- features/support/world.js
+++ features/support/world.js
@@ -1,3 +1,5 @@
+const EventEmitter = require('events');
+const sinon = require('sinon');
 const { setWorldConstructor } = require('cucumber');
 
 
@@ -22,12 +24,18 @@ function DreddWorld({ attach, parameters }) {
   this.apiDescriptionExt = API_DESCRIPTION_EXTS[this.apiDescriptionFormat];
   this.hooksExt = HOOKS_EXTS[this.apiDescriptionFormat];
 
-  this.dredd = {
-    apiDescription: null,
-    apiLocation: 'http://127.0.0.1:3000',
-    args: [],
-    output: '',
-    exitStatus: null,
+  // This is currently the best way the tests can spy on what Dredd is doing
+  // when it's running. We provide Dredd with a custom EventEmitter instance,
+  // on which it fires events, and we spy on the 'emit()' function to see what
+  // has been emitted and with which arguments.
+  this.events = new EventEmitter();
+  sinon.spy(this.events, 'emit');
+
+  this.config = {
+    emitter: this.events,
+    endpoint: 'http://127.0.0.1:3000',
+    loglevel: 'debug',
+    options: {},
   };
 }
 
