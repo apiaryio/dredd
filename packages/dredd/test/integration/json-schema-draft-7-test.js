56162429e2f414d495ab44cf4aabe5b9c2badf47
artem-zakharchenko <artem.zakharchenko@oracle.com>
2019-12-09 13:08:47 +0100

    test: adds JSON Schema boolean value integration test

--- packages/dredd/test/integration/json-schema-draft-7-test.js
+++ packages/dredd/test/integration/json-schema-draft-7-test.js
@@ -4,67 +4,108 @@ import Dredd from '../../lib/Dredd';
 import { runDreddWithServer, createServer } from './helpers';
 
 describe('Given API Blueprint with JSON Schema Draft 7', () => {
-  describe('given actual data matches the schema', () => {
-    let runtimeInfo;
+  describe('given explicit version of JSON Schema', () => {
+    describe('given actual data matches the schema', () => {
+      let runtimeInfo;
 
-    before((done) => {
-      const app = createServer();
-      app.get('/machines', (req, res) => {
-        res.json([{ type: 'bulldozer', name: 'willy' }]);
+      before((done) => {
+        const app = createServer();
+        app.get('/machines', (req, res) => {
+          res.json([{ type: 'bulldozer', name: 'willy' }]);
+        });
+
+        const dredd = new Dredd({
+          path: './test/fixtures/json-schema-draft-7.apib',
+        });
+
+        runDreddWithServer(dredd, app, (...args) => {
+          let error;
+          [error, runtimeInfo] = Array.from(args);
+          done(error);
+        });
       });
 
-      const dredd = new Dredd({
-        path: './test/fixtures/json-schema-draft-7.apib',
+      it('should output no failures or errors', () => {
+        assert.equal(
+          runtimeInfo.dredd.stats.failures + runtimeInfo.dredd.stats.errors,
+          0,
+        );
       });
 
-      runDreddWithServer(dredd, app, (...args) => {
-        let error;
-        [error, runtimeInfo] = Array.from(args);
-        done(error);
+      it('should result into passing test', () => {
+        assert.equal(runtimeInfo.dredd.stats.passes, 1);
       });
     });
 
-    it('should output no failures or errors', () => {
-      assert.equal(
-        runtimeInfo.dredd.stats.failures + runtimeInfo.dredd.stats.errors,
-        0,
-      );
-    });
+    describe('given actual data does not match the schema', () => {
+      let runtimeInfo;
 
-    it('should result into passing test', () => {
-      assert.equal(runtimeInfo.dredd.stats.passes, 1);
+      before((done) => {
+        const app = createServer();
+        app.get('/machines', (req, res) => {
+          res.json([{ type: 'unknown', name: 'willy' }]);
+        });
+
+        const dredd = new Dredd({
+          path: './test/fixtures/json-schema-draft-7.apib',
+        });
+
+        runDreddWithServer(dredd, app, (...args) => {
+          let error;
+          [error, runtimeInfo] = Array.from(args);
+          done(error);
+        });
+      });
+
+      it('should result into 1 failing test', () => {
+        assert.equal(runtimeInfo.dredd.stats.failures, 1);
+      });
+
+      it('should output an error about unknown "type" enum value', () => {
+        assert.match(
+          runtimeInfo.dredd.logging,
+          /data\/0\/type should be equal to one of the allowed values/,
+        );
+      });
     });
   });
 
-  describe('given actual data does not match the schema', () => {
-    let runtimeInfo;
+  describe('given a single Boolean value as a JSON Schema', () => {
+    describe('given schema equals "true"', () => {
+      let runtimeInfo;
 
-    before((done) => {
-      const app = createServer();
-      app.get('/machines', (req, res) => {
-        res.json([{ type: 'unknown', name: 'willy' }]);
+      before((done) => {
+        const app = createServer();
+        app.get('/machines/:id', (req, res) => {
+          const { id } = req.params
+          const machines = [{ type: 'bulldozer', name: 'willy' }]
+          res.json(machines[id] || {});
+        });
+
+        const dredd = new Dredd({
+          path: './test/fixtures/json-schema-draft-7-boolean.apib',
+        });
+
+        runDreddWithServer(dredd, app, (...args) => {
+          let error;
+          [error, runtimeInfo] = Array.from(args);
+          done(error);
+        });
       });
 
-      const dredd = new Dredd({
-        path: './test/fixtures/json-schema-draft-7.apib',
+      it('should recognize schema as Draft 7', () => {
+        // No invalid schema version error from Gavel and no failing tests
+        // implies the schema version was properly inferred as Draft 7.
+        assert.notMatch(runtimeInfo.dredd.logging, /not a valid draft/);
+        assert.equal(
+          runtimeInfo.dredd.stats.failures + runtimeInfo.dredd.stats.errors,
+          0,
+        );
       });
 
-      runDreddWithServer(dredd, app, (...args) => {
-        let error;
-        [error, runtimeInfo] = Array.from(args);
-        done(error);
+      it('should always result into passing test', () => {
+        assert.equal(runtimeInfo.dredd.stats.passes, 1);
       });
-    });
-
-    it('should result into 1 failing test', () => {
-      assert.equal(runtimeInfo.dredd.stats.failures, 1);
-    });
-
-    it('should output an error about unknown "type" enum value', () => {
-      assert.match(
-        runtimeInfo.dredd.logging,
-        /data\/0\/type should be equal to one of the allowed values/,
-      );
     });
   });
 });
